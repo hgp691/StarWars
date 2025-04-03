f245ae1d91edb41f565dff660b861e36f6398415
hgp691 <hgp691@gmail.com>
2025-04-03 12:57:02 -0500

    add fetcher and endpoints

---
+++ SWCore/SWCore.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SWCore.
+FOUNDATION_EXPORT double SWCoreVersionNumber;
+
+//! Project version string for SWCore.
+FOUNDATION_EXPORT const unsigned char SWCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWCore/PublicHeader.h>
